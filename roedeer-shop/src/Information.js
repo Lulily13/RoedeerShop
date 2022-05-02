beb8451055e3c7da4302a1f0467ea123c08a9dcd
Lulily13 <bloodyakuma13@yahoo.com>
2022-05-02 19:35:14 +0200

    Added map metod to Information component and styled Information section

--- roedeer-shop/src/Information.js
+++ roedeer-shop/src/Information.js
@@ -1,31 +1,47 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Information = () => {
+    const help = ["POMOC", "Regulaminy", "Rozmiary pierścionków", "Polityka prywatności","Jak dbać o biżuterię"];
+    const payment = ["PŁATNOŚĆ I WYSYŁKA", "Jak kupować", "Płatność i wysyłka", "Zwroty i reklamacje"];
+    const contact = ["KONTAKT", "Formularz kontaktowy", "e-mail: roedeer@gmail.com", "instagram: roedeer_art"];
+
+    const helpList = (
+        <ul className="information-list">
+            {
+                help.map(element => {
+                    return <Link to={element.replace(" ", "-")} key={help.indexOf(element)} className="information-element">{element}</Link>
+                })
+            }
+        </ul>
+    );
+    const paymentList = (
+        <ul className="information-list">
+            {
+                payment.map(element => {
+                    return <Link to={element.replace(" ", "-")} key={payment.indexOf(element)} className="information-element">{element}</Link>
+                })
+            }
+        </ul>
+    );
+
+    const contactList = (
+        <ul className="information-list">
+            {
+                contact.map(element => {
+                    return <li key={contact.indexOf(element)} className="information-element">{element}</li>
+                })
+            }
+        </ul>
+    );
+
     return (
-        <>
-            <div className="information">
-                    <ul className="help">
-                        <li>POMOC</li>
-                        <li>Regulaminy</li>
-                        <li>Rozmiary pierścionków</li>
-                        <li>Polityka prywatności</li>
-                        <li>Jak dbać o biżuterię</li>
-                    </ul>
-                    <ul className="payment">
-                        <li>PŁATNOŚĆ I WYSYŁKA</li>
-                        <li>Jak kupować</li>
-                        <li>Płatność i wysyłka</li>
-                        <li>Zwroty i reklamacje</li>
-                    </ul>
-                    <ul className="contact">
-                        <li>KONTAKT</li>
-                        <li>Formularz kontaktowy</li>
-                        <li>e-mail: roedeer@gmail.com</li>
-                        <li>tel. 666 666 666</li>
-                    </ul>
-            </div>
-        </>
-    )
+        <div className="information">
+            <ul className="help">{helpList}</ul>
+            <ul className="payment">{paymentList}</ul>
+            <ul className="contact">{contactList}</ul>
+        </div>
+    );
 }
 
 export default Information;
